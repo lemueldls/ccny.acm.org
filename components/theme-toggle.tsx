3f54d3b0586556b1abaf353f2f141278873c8250
Lemuel DLS <26912197+lemueldls@users.noreply.github.com>
2025-07-03 18:35:48 -0400

    fix: aria

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -21,9 +21,9 @@ export default function ThemeToggle() {
   return (
     <Button onPress={toggleTheme} color="default" variant="light" isIconOnly>
       {resolvedTheme === "dark" ? (
-        <SunIcon className="size-5" />
+        <SunIcon className="size-5" aria-label="Light Theme" />
       ) : (
-        <MoonIcon className="size-5" />
+        <MoonIcon className="size-5" aria-label="Dark Theme" />
       )}
     </Button>
   );
