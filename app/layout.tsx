7f2f8c5bd5a3825324a216e93be49b552a35a8ef
Lemuel DLS <noreply@git.lemueldls.dev>
2026-03-02 12:46:32 -0500

    chore: update icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,18 +41,18 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { rel: "icon", type: "image/svg+xml", url: "/favicon.svg" },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "32x32",
-        url: "/favicon-32x32.png",
-      },
-      {
-        rel: "icon",
-        type: "image/png",
-        sizes: "16x16",
-        url: "/favicon-16x16.png",
-      },
+      // {
+      //   rel: "icon",
+      //   type: "image/png",
+      //   sizes: "32x32",
+      //   url: "/favicon-32x32.png",
+      // },
+      // {
+      //   rel: "icon",
+      //   type: "image/png",
+      //   sizes: "16x16",
+      //   url: "/favicon-16x16.png",
+      // },
       {
         rel: "apple-touch-icon",
         type: "image/png",
