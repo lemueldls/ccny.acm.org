99a097ca452b5cabd9c38b967a67b6a9bb4a3146
Lemuel DLS <noreply@git.lemueldls.dev>
2026-06-03 20:56:45 -0400

    chore: update team

--- app/home/team.tsx
+++ app/home/team.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { EnvelopeIcon } from "@heroicons/react/20/solid";
+import { GlobeAltIcon } from "@heroicons/react/24/solid";
 import { Button, Card, CardBody, Image, Link } from "@heroui/react";
 import NextImage from "next/image";
 import Tilt from "react-parallax-tilt";
@@ -22,142 +23,110 @@ export interface TeamMember {
 
 export const executiveBoard: TeamMember[] = [
   {
-    email: "smondal002@citymail.cuny.edu",
-    image: "/members/srewashi-mondal.webp",
-    linkedin: "srewashi-mondal",
-    name: "Srewashi Mondal",
+    name: "Krista Singh",
     position: "President",
+    email: "krista.singh87@stu-mail.ccny.cuny.edu",
+    image: "/members/krista-singh.webp",
+    linkedin: "kristausingh",
   },
   {
-    email: "ehaque002@citymail.cuny.edu",
-    image: "/members/evan-haque.webp",
-    linkedin: "evanhaque1",
-    name: "Evan Haque",
+    name: "Lemuel De Los Santos",
     position: "Vice President",
-  },
-  {
-    email: "ldeloss002@citymail.cuny.edu",
-    github: "lemueldls",
+    email: "lemuel.delossantos22@stu-mail.ccny.cuny.edu",
     image: "/members/lemuel-de-los-santos.webp",
     linkedin: "lemueldls",
-    name: "Lemuel De Los Santos",
-    position: "Secretary",
+    github: "lemueldls",
     website: "https://lemueldls.dev",
   },
   {
-    email: "afredri000@citymail.cuny.edu",
+    name: "Axyl Fredrick",
+    position: "Secretary",
+    // email: "afredri000@citymail.cuny.edu",
+    email: "axylofredrick@gmail.com",
     image: "/members/axyl-fredrick.webp",
     linkedin: "axyl-fredrick",
-    name: "Axyl Fredrick",
+    github: "axylf",
+  },
+  {
+    name: "Zhuolin Li",
     position: "Treasurer",
+    email: "zhuolin.li75@stu-mail.ccny.cuny.edu",
+    image: "/members/zhuolin-li.webp",
+    linkedin: "zhuolin-li1",
   },
 ];
 
 export const extendedBoard: TeamMember[] = [
   {
-    email: "dlucero000@citymail.cuny.edu",
-    image: "/members/diana-lucero.webp",
-    linkedin: "dlucero0715",
-    name: "Diana Lucero",
+    name: "Zara Raza",
     position: "Social Media Chair",
+    email: "zara.raza77@stu-mail.ccny.cuny.edu",
+    image: "/members/zara-raza.webp",
+    linkedin: "zara-r-",
   },
   {
-    email: "dsen000@citymail.cuny.edu",
-    image: "/members/debasree-sen.webp",
-    linkedin: "debasree-sen",
-    name: "Debasree Sen",
+    name: "Tanzina Sumona",
     position: "Social Media Chair",
+    email: "tanzina.sumona77@stu-mail.ccny.cuny.edu",
+    image: "/members/tanzina-sumona.webp",
+    linkedin: "tanzina-sumona",
   },
   {
-    email: "ycheng004@citymail.cuny.edu",
-    image: "/members/yumi-cheng.webp",
-    linkedin: "yumicheng",
-    name: "Yumi Cheng",
+    name: "Kelvin Osei",
     position: "Marketing Chair",
+    email: "kelvin.osei34@stu-mail.ccny.cuny.edu",
+    image: "/members/kelvin-osei.webp",
+    linkedin: "kelvin-osei-084737373",
   },
   {
-    email: "arahama003@citymail.cuny.edu",
-    image: "/members/addina-rahaman.webp",
-    linkedin: "addina-rahaman-00049a2a2",
-    name: "Addina Rahaman",
-    position: "Sponsoring Chair",
+    name: "Thanjila Thahsin",
+    position: "Sponsorship Chair",
+    email: "thanjila.thahsin67@stu-mail.ccny.cuny.edu",
+    image: "/members/thanjila-thahsin.webp",
+    linkedin: "thanjila",
+    website: "https://thanjila.com",
   },
   {
-    email: "ttan001@citymail.cuny.edu",
-    image: "/members/timson-tan.webp",
-    linkedin: "timsontan",
-    name: "Timson Tan",
-    position: "Community Manager",
+    name: "Mohmed Bemat",
+    position: "Sponsorship Chair",
+    email: "mohmed.bemat17@stu-mail.ccny.cuny.edu",
+    image: "/members/mohmed-bemat.webp",
+    linkedin: "mohmedbemat",
   },
   {
-    email: "adeepaw000@citymail.cuny.edu",
-    image: "/members/arnav-deepaware.webp",
-    linkedin: "arnavdeepaware",
-    name: "Arnav Deepaware",
-    position: "Workshop Manager\nClass Representative 2027",
+    name: "Xanath Hernandez",
+    position: "Graphic Design Chair",
+    email: "xanath.hernandez62@stu-mail.ccny.cuny.edu",
+    image: "/members/xanath-hernandez.webp",
+    linkedin: "xanath-hernandez-807988318",
   },
   {
-    email: "ksingh020@citymail.cuny.edu",
-    image: "/members/krista-singh.webp",
-    linkedin: "kristausingh",
-    name: "Krista Singh",
-    position: "Graphics Chair",
-  },
-  {
-    email: "milyas001@citymail.cuny.edu",
-    image: "/members/maryam-ilyas.webp",
-    linkedin: "maryamilyas",
-    name: "Maryam Ilyas",
+    name: "Azeez Siyanbola",
     position: "Newsletter Chair",
+    email: "azeez.siyanbola39@stu-mail.ccny.cuny.edu",
+    // image: "/members/azeez-siyanbola.webp",
+    linkedin: "azeez-siyanbola-062b542a8",
   },
   {
-    email: "ajha001@citymail.cuny.edu",
-    image: "/members/aditya-jha.webp",
-    linkedin: "aditya-jha777",
-    name: "Aditya Jha",
-    position: "Outreach Chair",
+    name: "Tamim Kabir",
+    position: "Class Representative 2028",
+    // email: "tkabir002@citymail.cuny.edu",
+    image: "/members/tamim-kabir.webp",
+    linkedin: "tamim-kabir",
   },
   {
-    email: "aali064@citymail.cuny.edu",
-    image: "/members/areeba-ali.webp",
+    name: "Vic Chen",
+    position: "Class Representative 2029",
+    email: "vicchen199@gmail.com",
+    image: "/members/vic-chen.webp",
+    linkedin: "vic-chen-cs",
+  },
+  {
     name: "Areeba Ali",
-    position: "Class Representative 2028",
+    position: "Outreach Chair",
+    email: "areeba.ali41@stu-mail.ccny.cuny.edu",
+    image: "/members/areeba-ali.webp",
   },
-  {
-    email: "ailyas000@citymail.cuny.edu",
-    image: "/members/ayesha-ilyas.webp",
-    linkedin: "a-ilyas",
-    name: "Ayesha Ilyas",
-    position: "Class Representative 2028",
-  },
-  // {
-  //   Name: "Jawad Chowdhury",
-  //   Position: "Treasurer",
-  //   Image: "/members/jawad-chowdhury.webp",
-  //   Email: "jchowdh002@citymail.cuny.edu",
-  //   Linkedin: "jawad-chy",
-  // },
-  // {
-  //   Name: "Lily Minchala",
-  //   Position: "Marketing Designer",
-  //   // image: "/members/lily-minchala.webp",
-  //   Email: "lmincha000@citymail.cuny.edu",
-  //   Linkedin: "lilyminchala",
-  // },
-  {
-    email: "sabrar000@citymail.cuny.edu",
-    image: "/members/sehr-abrar.webp",
-    linkedin: "sehr-abrar",
-    name: "Sehr Abrar",
-    position: "Student Advisor",
-  },
-  // {
-  //   Name: "Daniel Chen",
-  //   Position: "Student Advisor",
-  //   Image: "/members/daniel-chen.webp",
-  //   Email: "dchen024@citymail.cuny.edu",
-  //   Linkedin: "daniel-chen297",
-  // },
 ];
 
 export const staff: TeamMember[] = [
@@ -178,11 +147,15 @@ export default function HomePageTeam() {
       ({
         "@type": "Person",
         email: member.email,
-        image: member.image ? `${process.env.NEXT_PUBLIC_ROOT_URL}${member.image}` : undefined,
+        image: member.image
+          ? `${process.env.NEXT_PUBLIC_ROOT_URL}${member.image}`
+          : undefined,
         jobTitle: member.position,
         name: member.name,
         sameAs: [
-          member.linkedin ? `https://www.linkedin.com/in/${member.linkedin}` : undefined,
+          member.linkedin
+            ? `https://www.linkedin.com/in/${member.linkedin}`
+            : undefined,
           member.github ? `https://github.com/${member.github}` : undefined,
           member.website,
         ].filter(Boolean) as string[],
@@ -245,7 +218,11 @@ function TeamMemberCard({ member }: { member: TeamMember }) {
 
   return (
     <Tilt className="flex-1 shrink basis-full md:basis-106 lg:max-w-120">
-      <Card isBlurred className="brick-wall !bg-default/15 border-none p-2" shadow="md">
+      <Card
+        isBlurred
+        className="brick-wall bg-default/15! border-none p-2"
+        shadow="md"
+      >
         <CardBody className="grid grid-cols-[fit-content(8rem)_1fr] grid-rows-[fit-content(8rem)_1fr] gap-x-5 overflow-visible">
           <Image
             as={NextImage}
@@ -270,45 +247,50 @@ function TeamMemberCard({ member }: { member: TeamMember }) {
             </span>
           </div>
 
-          <div className="col-span-2 flex items-end justify-end gap-2 sm:col-span-1">
-            {/* {member.website && (
-          <Button
-            as={Link}
-            href={member.website}
-            title="Website"
-            variant="light"
-            isExternal
-            isIconOnly
-          >
-            <GlobeAltIcon className="h-5 w-5" />
-          </Button>
-        )} */}
+          <div className="col-span-2 flex items-end justify-end gap-4 sm:col-span-1">
+            <div className="flex items-center justify-end gap-2">
+              {member.website && (
+                <Button
+                  as={Link}
+                  href={member.website}
+                  title="Website"
+                  variant="light"
+                  className="text-default-foreground"
+                  isExternal
+                  isIconOnly
+                >
+                  <GlobeAltIcon className="h-6 w-6" />
+                </Button>
+              )}
 
-            {member.github && (
-              <Button
-                as={Link}
-                href={`https://github.com/${member.github}`}
-                title="GitHub"
-                variant="light"
-                isExternal
-                isIconOnly
-              >
-                <SimpleIconsGithub className="h-5 w-5" />
-              </Button>
-            )}
+              {member.github && (
+                <Button
+                  as={Link}
+                  href={`https://github.com/${member.github}`}
+                  title="GitHub"
+                  variant="light"
+                  className="text-default-foreground"
+                  isExternal
+                  isIconOnly
+                >
+                  <SimpleIconsGithub className="h-5 w-5" />
+                </Button>
+              )}
 
-            {member.linkedin && (
-              <Button
-                href={`https://linkedin.com/in/${member.linkedin}/`}
-                as={Link}
-                title="LinkedIn"
-                variant="light"
-                isExternal
-                isIconOnly
-              >
-                <SimpleIconsLinkedin className="h-5 w-5" />
-              </Button>
-            )}
+              {member.linkedin && (
+                <Button
+                  href={`https://linkedin.com/in/${member.linkedin}/`}
+                  as={Link}
+                  title="LinkedIn"
+                  variant="light"
+                  className="text-default-foreground"
+                  isExternal
+                  isIconOnly
+                >
+                  <SimpleIconsLinkedin className="h-5 w-5" />
+                </Button>
+              )}
+            </div>
 
             {member.email && (
               <Button
